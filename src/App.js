eeabc6699ccbd58de03fff754e69f55f0c16862a
max <max.smith@duke.edu>
2020-09-06 23:44:29 -0400

    Updated images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Mason from './components/Mason';
-import Header from './components/Header';
+// import Header from './components/Header';
 
 function App() {
   return (
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="Header">
+      <h1>Welcome to Team Big Eats!</h1>
+      {/* <h2>Max Smith's Personal Food Blog :)</h2> */}
+    </header>
+  );
+}
+
+export default Header;
