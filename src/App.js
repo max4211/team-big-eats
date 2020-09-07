c5b35434b4feef188c0092c01475c7bba21550dd
max <max.smith@duke.edu>
2020-09-06 23:37:07 -0400

    Added first bout of food

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Mason from './components/Mason';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      {/* <Header/> */}
       <Mason/>
     </div>
   );
--- src/components/Mason.js
+++ src/components/Mason.js
@@ -1,27 +1,58 @@
 import React from "react";
 import "./Mason.css";
 import Masonry from "react-masonry-css";
-import "../img/IMG_1.JPEG";
+
+/* PHOTO IMPORTS*/
+import Lava from "../img/IMG_50.JPEG";
+import Scallops from "../img/IMG_115.JPEG";
+import Omlette from "../img/IMG_114.JPEG";
+import Tacos from "../img/IMG_113.JPEG";
+import Steak from "../img/IMG_112.JPEG";
+import Sandwhich from "../img/IMG_111.JPEG";
+import Salad from "../img/IMG_110.JPEG";
+import Cookies from "../img/IMG_109.JPEG";
+import Shrimp from "../img/IMG_108.JPEG";
+import SteakSandwhich from "../img/IMG_107.JPEG";
+import LambRack from "../img/IMG_106.JPEG";
+import EggsHam from "../img/IMG_105.JPEG";
+import AvoToast from "../img/IMG_104.JPEG";
+import Waffles from "../img/IMG_103.JPEG";
+import Pho from "../img/IMG_102.JPEG";
+import Ramen from "../img/IMG_101.JPEG";
+
+
 
 
 function Mason() {
 
-    var maxWidth = 300;
+    var maxWidth = "500px";
 
     var items = [
-        {id: 1, src: "../img/IMG_1.JPEG", alt: "Ramen"},
-        {id: 2, src: "../img/IMG_2.JPEG", alt: "Ramen"},
-        {id: 3, src: "../img/IMG_3.JPEG", alt: "Ramen"},
-        {id: 4, src: "../img/IMG_4.JPEG", alt: "Ramen"}
+        {id: 1, src: Ramen, alt: "Ramen"},
+        {id: 2, src: Pho, alt: "Pho"},
+        {id: 3, src: Waffles, alt: "Waffles"},
+        {id: 4, src: AvoToast, alt: "AvoToast"},
+        {id: 5, src: EggsHam, alt: "EggsHam"},
+        {id: 6, src: LambRack, alt: "LambRack"},
+        {id: 7, src: SteakSandwhich, alt: "SteakSandwhich"},
+        {id: 8, src: Shrimp, alt: "Shrimp"},
+        {id: 9, src: Cookies, alt: "Cookies"},
+        {id: 10, src: Salad, alt: "Salad"},
+        {id: 11, src: Sandwhich, alt: "Sandwhich"},
+        {id: 12, src: Steak, alt: "Steak"},
+        {id: 13, src: Tacos, alt: "Tacos"},
+        {id: 14, src: Omlette, alt: "Omlette"},
+        {id: 15, src: Scallops, alt: "Scallops"},
+        // {id: 16, src: Lava, alt: "Lava"}
     ];
       
       // Convert array to JSX items
       items = items.map(function(item) {
-        return <img key={item.id} src={require(item.src)} alt={item.alt} width={maxWidth}/>
+        return <img key={item.id} src={item.src} alt={item.alt} width={maxWidth} className="food-photo"/>
       });
       
   return (
-    <div className="App">
+    <div className="mason-wrapper">
       <Masonry
         breakpointCols={3}
         className="my-masonry-grid"
