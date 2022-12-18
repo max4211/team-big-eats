a68a55add0b3e9fa8de1688959b689ce00338651
Max Smith <ms724@duke.edu>
2022-12-18 18:55:35 -0500

    udpates ostarr photo formatting

--- src/pages/Countdown.js
+++ src/pages/Countdown.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Countdown from 'react-countdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SFPhoto from "../img/ostarr_masmith.jpg";
 
   
 const About = () => {
     const date = Date.parse("24 Dec 2022 13:30:00 PST")
   return (
-    <div>
+    <div style={{ display: 'grid', margin: '20px'}}>
         <h2>Countdown until ostarr/masmith hang in SF!</h2>
       <Countdown date={date} />
+      <img src={SFPhoto} alt="ostarr_masmith_photo" style={{ maxHeight:'70%', maxWidth: '40%' }} />
     </div>
   );
 };
