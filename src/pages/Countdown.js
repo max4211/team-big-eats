7b2504ebec1cf4f576ce502e65ec0512bde97b0a
Max Smith <ms724@duke.edu>
2022-12-18 19:00:09 -0500

    fixes some styling; reconnects aws

--- src/pages/Countdown.js
+++ src/pages/Countdown.js
@@ -7,10 +7,10 @@ import SFPhoto from "../img/ostarr_masmith.jpg";
 const About = () => {
     const date = Date.parse("24 Dec 2022 13:30:00 PST")
   return (
-    <div style={{ display: 'grid', margin: '20px'}}>
+    <div style={{ display: 'grid', margin: 'auto', background: '#EFEFEF', width: '100%', padding: '20px', aligntItems: 'center'}}>
         <h2>Countdown until ostarr/masmith hang in SF!</h2>
-      <Countdown date={date} />
-      <img src={SFPhoto} alt="ostarr_masmith_photo" style={{ maxHeight:'70%', maxWidth: '40%' }} />
+      <Countdown date={date} style={{ margin: 'auto'}}/>
+      <img src={SFPhoto} alt="ostarr_masmith_photo" style={{ maxHeight:'90%', maxWidth: '100%', margin: 'auto' }} />
     </div>
   );
 };
