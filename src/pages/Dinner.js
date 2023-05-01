bc1de6112deb8ece9a14a3a5e7d9acc327405c88
Max Smith <ms724@duke.edu>
2023-05-01 07:08:09 -0400

    adds initial menu, todo styling

--- src/pages/Dinner.js
+++ src/pages/Dinner.js
@@ -1,11 +1,47 @@
 import React from "react";
-import "./Test.css";
+import "./Dinner.css";
 
 function Dinner() {
 
   return (
     <div className="dinner-wrapper">
-      <h1>Dinner Menu</h1>
+        <div className="dinner-menu">
+            <h1>Dinner Menu</h1>
+            <div className="d-section">
+                <h3 className="section-name">Drinks</h3>
+                <div className="menu-item">
+                    <p className="strong-name">Red Wine</p>
+                    <p className="light-desc">TODO description</p>
+                </div>
+            </div>
+            <div className="d-section">
+                <h3 className="section-name">Main</h3>
+                <div className="menu-item">
+                    <p className="strong-name">Steak</p>
+                    <p className="light-desc">Sous vide filet, truffle butter</p>
+                </div>
+                <div className="menu-item">
+                    <p className="strong-name">Potatoes</p>
+                    <p className="light-desc">Kenji's crispy potatoes, rosemary/thyme oil</p>
+                </div>
+                <div className="menu-item">
+                    <p className="strong-name">Broccoli</p>
+                    <p className="light-desc">Gotta have a vegetable, lemon and garlic</p>
+                </div>
+            </div>
+            <div className="d-section">
+                <h3 className="section-name">Dessert</h3>
+                <div className="menu-item">
+                    <p className="strong-name">Chocolate Lava Cakes</p>
+                    <p className="light-desc">Vanilla ice cream, berries</p>
+                </div>
+            </div>
+            <div className="d-footer">
+                <p className="p-footer">Executive Chef Max Smith</p>
+            </div>
+
+        </div>
+      
     </div>
   );
 }
